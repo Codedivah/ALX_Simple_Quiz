4a0c86c3c41639f339eace50bb8748970770174c
Fatimah Badmus <fatimahbadmus04@gmail.com>
2025-06-13 21:31:48 +0100

    Implement quiz answer checking functionality and add live server settings

--- quiz.js
+++ quiz.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function checkAnswer() {
+    const correctAnswer="4";
+    const  feedback= document.getElementById("feedback");
+// Clear the input field
+    /*feedback.textContent = "Checking your answer...";*/
+
+        
+
+   const userAnswer= document.querySelector('input[name="quiz"]:checked').value;
+    if (userAnswer === correctAnswer) {
+        feedback.textContent= ("Correct! Well done.");
+    } else {
+        feedback.textContent= ("That's incorrect. Try again!");
+    }
+}
+
+   const submitButton= document.getElementById('submit-answer'); 
+    submitButton.addEventListener('click',checkAnswer);
